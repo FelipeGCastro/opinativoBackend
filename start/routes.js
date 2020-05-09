37a0425ab016776f453e10c08d3844eb018e277f
Luiz Felipe Castro <luiz@Kenetto-MBP-13.local>
2020-05-09 05:11:19 +0100

    change to public route with topics index

--- start/routes.js
+++ start/routes.js
@@ -7,11 +7,12 @@ Route.post('sessions', 'SessionController.store')
 // sourcer can have param ?source=:source_id
 Route.get('links', 'LinkController.index')
 Route.get('sources', 'SourceController.index')
+Route.get('topics', 'SourceController.index')
 Route.get('links/:id', 'LinkController.show')
 
 Route.group(() => {
   Route.post('users', 'UserController.store')
   Route.resource('sources', 'SourceController').except(['index']).apiOnly()
-  Route.resource('topics', 'TopicController').apiOnly()
+  Route.resource('topics', 'TopicController').except(['index']).apiOnly()
   Route.resource('links', 'LinkController').except(['index', 'show']).apiOnly()
 }).middleware(['auth'])
